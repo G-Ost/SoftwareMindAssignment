5459c79c612c921379d63f23a5281ec59524a7c2
G-Ost <g.ostapczuk@gmail.com>
2024-08-01 19:25:59 +0200

    User router rename to Form

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import continentRouter from "./routes/continentRouter";
-import userRouter from "./routes/usersRouter";
+import formRouter from "./routes/formRouter";
 import sequelize from "./config/database";
+
 sequelize.sync({ force: true }).then(() => {
   console.log("db is ready");
 });
@@ -10,7 +11,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use("/api/continent", continentRouter);
-app.use("/api/users", userRouter);
+app.use("/api/form", formRouter);
 
 app.listen(3000, () => {
   console.log("app is running");
--- server/src/routes/formRouter.ts
+++ server/src/routes/formRouter.ts
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { createUser, getUsers } from "../controllers/usersController";
+import { createUser, getUsers } from "../controllers/formController";
 
 router.route("/").post(createUser).get(getUsers);
 
